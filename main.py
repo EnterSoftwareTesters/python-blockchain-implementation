d234bdce1505d9ef3183eaf388243c2227f370d8
pushp1997 <pushptyagi1@gmail.com>
2023-05-12 09:45:01 +0100

    Added driver code

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.chain import Chain
+
+chain = Chain(20)
+
+while True:
+    data = input("Add something to the chain: ")
+    chain.add_to_pool(data)
+    chain.mine()
+    print("Updated Blockchain:")
+    for block in chain.blocks:
+        print("*" * 50)
+        print(f"Hash:\t\t{block.hash.hexdigest()}")
+        print(f"Previous Hash:\t{block.prev_hash.hexdigest()}")
+        print(f"Nonce:\t\t{block.nonce}")
+        print(f"Data:\t\t{block.data}")
+        print("*" * 50)
