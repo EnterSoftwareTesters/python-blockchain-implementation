966a77cd733b67c825812519010c9909668b260a
pushp1997 <pushptyagi1@gmail.com>
2023-05-12 09:44:05 +0100

    Added Proof of Work, add to chain and add to pool functionality

--- src/chain.py
+++ src/chain.py
@@ -1,15 +1,38 @@
-from hashlib import _Hash, sha256
-from typing import List
-from block import Block
+from __future__ import annotations
+import hashlib
+from typing import List, Any
+from .block import Block
 
 
 class Chain:
     def __init__(self, difficulty: int) -> None:
         self.difficulty: int = difficulty
-        self.blocks: List[_Hash] = []
-        self.pool: List[_Hash] = []
+        self.blocks: List[hashlib._Hash] = []
+        self.pool: List[hashlib._Hash] = []
+        self.create_origin_block()
 
     def create_origin_block(self) -> None:
-        origin = Block(data="", prev_hash=sha256(str("0000000000000000").encode("utf-8")))
+        origin = Block(data="Origin", prev_hash=hashlib.sha256(str("0" * 16).encode("utf-8")))
         origin.mine(self.difficulty)
         self.blocks.append(origin)
+
+    def proof_of_work(self, block: Block):
+        return (
+            block.hash.hexdigest() == hash.hexdigest()
+            and int(hash.hexdigest(), 16) < 2 ** (256 - self.difficulty)
+            and block.prev_hash == self.blocks[-1].hash
+        )
+
+    def add_to_chain(self, block: Block):
+        if self.proof_of_work(block):
+            self.blocks.append(block)
+
+    def add_to_pool(self, data: Any):
+        self.pool.append(data)
+
+    def mine(self):
+        if len(self.pool) > 0:
+            data = self.pool.pop(0)
+            block = Block(data, self.blocks[-1].hash)
+            block.mine(self.difficulty)
+            self.add_to_chain(block)
